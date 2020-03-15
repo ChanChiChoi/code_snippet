39d7cd5821c1c67c84e48a39f63be450a7a56d5f
ChanChiChoi <342888106@qq.com>
2020-03-15 20:49:41 +0800

    create

---
+++ cuda/app_design_dev/xorNM/nelmin.h
@@ -0,0 +1,14 @@
+#ifndef NELMIN_H
+#define NELMIN_H
+
+// Nelder-Mead Minimization Algorithm ASA047
+// from the Applied Statistics Algorithms available
+// in STATLIB. Adapted from the C version by J. Burkhardt
+// http://people.sc.fsu.edu/~jburkardt/c_src/asa047/asa047.html
+
+
+
+
+
+
+#endif
