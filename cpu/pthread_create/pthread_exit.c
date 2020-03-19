fb9e3a00178abebf01e0be03ef36392b55b9fbb5
ChanChiChoi <342888106@qq.com>
2020-03-19 11:31:17 +0800

    update

---
+++ cpu/pthread_create/pthread_exit.c
@@ -0,0 +1,26 @@
+#include<pthread.h>
+#include<stdio.h>
+
+void other_function(){
+  pthread_exit(NULL);
+}
+
+void *entry_point(void *arg){
+  printf("hello world\n");
+  other_function();
+  printf("can you see me?\n");
+  return NULL;
+}
+int main(int argc, char *argv[]){
+  pthread_t tidp;
+  if(pthread_create(&tidp, NULL, &entry_point, NULL)){
+    printf("could not create thread\n");
+    return -1;
+  }
+
+  if(pthread_join(tidp, NULL)){
+    printf("could not join thread\n");
+    return -1;
+  }
+  return 0;
+}
