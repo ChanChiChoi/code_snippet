d4779e8082b39e7200f7dd97121eeaa4894220c7
ChanChiChoi <342888106@qq.com>
2020-04-13 16:12:44 +0800

    update

--- cpu/pthread_create/pthread_barrier.c
+++ cpu/pthread_create/pthread_barrier.c
@@ -1,20 +1,4 @@
 /*
-==========================================
-This program spawns a number of threads, assigning each to compute part of a matrix multiplication. Each thread then uses the result of that computation in the next phase: another matrix multiplication.
-
-There are a few things to note here:
-
-1-The barrier declaration at the top
-2-The barrier initialization in main
-3-The point where each thread waits for its peers to finish.
-
-NOTE:
-The preprocessor definition of _XOPEN_SOURCE at the top of the program is important; without it, the barrier prototypes are not defined in pthread.h. The definition must come before any headers are included.
-
-====================================
-*/
-
-/*
  *
  * 障碍同步：在执行某个任务前，必须完成N个线程各自的任务，才能往下执行。主要有 pthread_barrier_wait 完成。
  *
@@ -28,65 +12,32 @@ The preprocessor definition of _XOPEN_SOURCE at the top of the program is import
 #include<stdio.h>
 #include<stdlib.h>
 
-#define ROWS 10000
-#define COLS 10000
-#define THREADS 10
-
-double initial_matrix[ROWS][COLS];
-double final_matrix[ROWS][COLS];
-
-pthread_barrier_t barr;
-
-extern void
-DotProduct(int row, int col,
-           double src[ROWS][COLS],
-           double dst[ROWS][COLS]);
-
-extern double
-Determinant(double matrix[ROWS][COLS]);
-
-void *entry_point(void *arg){
-
-  int rank = *(int*)arg;
-  for(int row = rank*ROWS/THREADS; row<(rank+1)*THREADS; ++row)
-    for(int col=0; col<COLS;++col)
-
-      DotProduct(row,col,initial_matrix,final_matrix);
-
-  //Synchronization point
-  int rc = pthread_barrier_wait(&barr);
-  if(rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD){
-    printf("could not wait on barrier\n");
-    exit(-1);
-  }
-
-  for(int row = rank*ROWS/THREADS; row<(rank+1)*THREADS; ++row)
-    for(int col=0;col<COLS;++col)
-     DotProduct(row,col,final_matrix, initial_matrix);
+#include <pthread.h>
+#include <stdio.h>
+ 
+pthread_barrier_t b;
+ 
+void* task(void* param) {
+    int id = *(int*) param;
+    printf("before the barrier %d\n", id);
+    pthread_barrier_wait(&b);
+    printf("after the barrier %d\n", id);
 }
+ 
+int main() {
+    int nThread = 5;
+    int i;
+ 
+    pthread_t thread[nThread];
+    pthread_barrier_init(&b, 0, nThread);
 
-int main(int argc, char*argv[]){
-  pthread_t tidp[THREADS];
-  
-  //barrier init
-  if(pthread_barrier_init(&barr,NULL,THREADS)){
-    printf("could not create a barrier\n");
-    return -1;
-  }
-  for(int i=0; i<THREADS; ++i)
-    if(pthread_create(&tidp[i],NULL,&entry_point, (void*)i)){
-      printf("could not create thread %d\n",i);
-      return -1;
-    }
+    for (i = 0; i < nThread; i++)
+        pthread_create(&thread[i], 0, task, (void*)&i);
 
-  for(int i=0; i<THREADS;++i)
-    if(pthread_join(tidp[i],NULL)){
-      printf("could not join thread %d\n",i);
-      return -1;
-    }
+    for (i = 0; i < nThread; i++)
+        pthread_join(thread[i], 0);
 
-  double det = Determinant(initial_matrix);
-  printf("the determinant of M^4 = %f\n",det);
+    pthread_barrier_destroy(&b);
+    return 0;
 
-  return 0;
 }
