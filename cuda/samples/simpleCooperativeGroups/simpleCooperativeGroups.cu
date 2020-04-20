3737f746e7b48839da82b72b24fde42f36813f94
ChanChiChoi <342888106@qq.com>
2020-04-20 11:09:53 +0800

    create

---
+++ cuda/samples/simpleCooperativeGroups/simpleCooperativeGroups.cu
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<cooperative_groups.h>
+
+#include<helper_cuda.h>
+
+using namespace cooperative_groups;
+
+
+__device__ int 
+sumReduction(thread_group g, int *x, int val){
+  //获取当前线程的索引
+  int lane = g.thread_rank();
+
+  for(int i = g.size()/2; i>0 ;i /= 2){
+    x[lane] = val;
+    g.sync();
+
+    if(lane<i){
+      val += x[lane +i];
+    }
+    g.sync();
+  }
+
+  if(lane == 0)
+    return val;
+  else
+    return -1;
+}
+
+__global__ void
+cgKernel(){
+  //获取当前块内所有线程
+  thread_block threadBlockGroup = this_thread_block();
+  int threadBlockGroupSize = threadBlockGroup.size();
+
+  extern __shared__ int workspace[];
+
+  int input, output, expectedOutput;
+
+  //获取当前线程在块中的索引
+  input = threadBlockGroup.thread_rank();
+  int cgTid = input;
+  
+  //首项加末项乘以项数(0 + n-1)*(n)/2
+  expectedOutput = (threadBlockGroupSize-1)*threadBlockGroupSize/2;
+  
+  output = sumReduction(threadBlockGroup, workspace, input);
+
+  if(cgTid == 0){
+    printf("index inside this group from 0 to %d, result is %d, expected is %d\n",
+             threadBlockGroupSize-1, output, expectedOutput);
+    printf(" need created %d groups, each group has 16 threads\n",threadBlockGroupSize/16);
+  }
+
+  threadBlockGroup.sync();
+  //将当前group以每个16个线程进行划分
+  thread_block_tile<16> tiledPartition16 = tiled_partition<16>(threadBlockGroup);
+  //整个group中当前线程索引，减去分块之后线程内部索引，？？
+  int workspaceOffset = threadBlockGroup.thread_rank()-tiledPartition16.thread_rank();
+  
+  input = tiledPartition16.thread_rank();
+  expectedOutput = 15*16/2;
+  output = sumReduction(tiledPartition16, workspace+workspaceOffset, input);
+
+  cgTid = input;
+  if(cgTid == 0){
+    printf("index inside this titledPartition16 group from 0 to 15, result is %d, expected is %d\n",output, expectedOutput);
+  }
+  
+}
+
+
+int
+main(){
+  
+  int blocksPerGrid = 1;
+  int threadsPerBlock = 64;
+  cgKernel<<<blocksPerGrid, threadsPerBlock,threadsPerBlock*sizeof(int)>>>();
+  checkCudaErrors(cudaDeviceSynchronize());
+  return 0;
+}
