7cce45dac2eb1df721dc578d4d88ab53d594bff8
ChanChiChoi <342888106@qq.com>
2020-05-04 09:43:29 +0800

    create

---
+++ cpu/cmake/simpleControllCompilerFlags/display.cpp
@@ -0,0 +1,7 @@
+#include"display.h"
+#include<iostream>
+using namespace std;
+
+void print(){
+  cout<<"hello print"<<endl;
+}
---
+++ cpu/cmake/simpleControllCompilerFlags/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"display.h"
+using namespace std;
+
+int main(){
+
+  print();
+}
