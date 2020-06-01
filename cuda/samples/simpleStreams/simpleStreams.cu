4586232741d634a19b1f1195c13fc904db8708eb
ChanChiChoi <342888106@qq.com>
2020-06-01 09:59:07 +0800

    update

--- cuda/samples/simpleStreams/simpleStreams.cu
+++ cuda/samples/simpleStreams/simpleStreams.cu
@@ -91,6 +91,32 @@ allocateHostMemory(bool bPinGenericMem, int **pp_a,
   }
 }
 
+bool
+correct_data(int *a, int const n, int const c){
+  for(int i=0; i<n; i++){
+    if(a[i] != c){
+     cout<<i<<", "<<a[i]<<" "<<c<<endl;
+     exit(EXIT_FAILURE);
+    }
+  }
+  return true;
+}
+
+inline void
+freeHostMemory(bool bPinGenericMem, int **pp_a, int **ppAligned_a, int nbytes){
+#if CUDART_VERSION >= 4000
+  if(bPinGenericMem){
+    checkCudaErrors(cudaHostUnregister(*ppAligned_a));
+    mumap(*pp_a, nbytes);
+  }
+  else
+#endif
+  {
+   cudaFreeHost(*pp_a);
+  }
+
+}
+
 int
 main(int argc, char *argv[]){
   int nstreams = 4;
@@ -98,12 +124,11 @@ main(int argc, char *argv[]){
   int n = 16*1024*1024;
   int nbytes = n*sizeof(int);
   dim3 threads,blocks;
-  float epalsed_time, time_memcpy, time_kernel;
+  float elapsed_time, time_memcpy, time_kernel;
   float scale_factor = 1.0f;
 
   int device_sync_method = cudaDeviceBlockingSync; //默认同步方式
   int niters;
-
   
   int cuda_device=0;
   checkCudaErrors(cudaSetDevice(cuda_device));
@@ -117,11 +142,98 @@ main(int argc, char *argv[]){
   int *h_a = nullptr;
   int *hAligned_a = nullptr; //指向已经对齐到MEMORY_ALIGNMENT的host mem
 
-//  allocateHostMemory(bPinGenericMem, &h_a, &hAligned_a, nbytes);
-  
-  
-  
-  
+  allocateHostMemory(bPinGenericMem, &h_a, &hAligned_a, nbytes);
 
+  // 分配device 内存，然后初始化
+  int *d_a = nullptr;
+  int *d_c = nullptr;
+  checkCudaErrors(cudaMalloc((void**)&d_a, nbytes));
+  checkCudaErrors(cudaMemset(d_a, 0x0, nbytes));
+  checkCudaErrors(cudaMalloc((void**)&d_c, sizeof(int)));
+  checkCudaErrors(cudaMemcpy(d_c, &c, sizeof(int), cudaMemcpyHostToDevice));
+
+  cout<<"Starting Test..."<<endl;
+  
+  //分配和初始化stream handles的数组
+  cudaStream_t *streams = (cudaStream_t*)malloc(nstreams * sizeof(cudaStream_t));
+  for(int i=0; i<nstreams; i++){
+    checkCudaErrors(cudaStreamCreate(&(streams[i])));
+  }
+  
+  //创建cuda event; 使用blocking sync
+  cudaEvent_t st, ed;
+  int eventFlags = (device_sync_method == cudaDeviceBlockingSync) ? \
+                   cudaEventBlockingSync : cudaEventDefault;
+  checkCudaErrors(cudaEventCreateWithFlags(&st, eventFlags));
+  checkCudaErrors(cudaEventCreateWithFlags(&ed, eventFlags));
+  
+  //计算device 到 host的memcpy时间
+  checkCudaErrors(cudaEventRecord(st,0));// 放置在第0个流中，以此保证之前的cuda call都完成了
+  checkCudaErrors(cudaMemcpyAsync(hAligned_a, d_a, nbytes, cudaMemcpyDeviceToHost, streams[0]));
+  checkCudaErrors(cudaEventRecord(ed,0));
+  checkCudaErrors(cudaEventSynchronize(ed));
+  checkCudaErrors(cudaEventElapsedTime(&time_memcpy, st, ed));
+  cout<<"memcopy: "<<time_memcpy<<endl;
+
+  //计算kernel的时间
+  threads = dim3(512);
+  blocks = dim3(n/threads.x);
+  checkCudaErrors(cudaEventRecord(st,0));
+  init_array<<<blocks, threads, 0, streams[0]>>>(d_a, d_c, niters);
+  checkCudaErrors(cudaEventRecord(ed,0));
+  checkCudaErrors(cudaEventSynchronize(ed));
+  checkCudaErrors(cudaEventElapsedTime(&time_kernel, st, ed));
+  cout<<"kernel: "<<time_kernel<<endl;
+  
+//===========================================
+  threads = dim3(512);
+  blocks = dim3(n/threads.x);
+  checkCudaErrors(cudaEventRecord(st,0));
+  for(int k=0; k<nreps; k++){
+    init_array<<<blocks, threads>>>(d_a, d_c, niters);
+    checkCudaErrors(cudaMemcpy(hAligned_a, d_a, nbytes, cudaMemcpyDeviceToHost));
+  }
+  checkCudaErrors(cudaEventRecord(ed,0));
+  checkCudaErrors(cudaEventSynchronize(ed));
+  checkCudaErrors(cudaEventElapsedTime(&elapsed_time, st, ed));
+  cout<<"不采用多流方式: "<<elapsed_time<<endl;
+
+//==========================================  
+  threads = dim3(512);
+  blocks = dim3(n/(nstreams*threads.x) );
+  memset(hAligned_a, 255, nbytes);//将host的内存都设为1，为了后续结果正确性测试
+  checkCudaErrors(cudaMemset(d_a, 0, nbytes));//将device内存设为0，为了后续测试
+  checkCudaErrors(cudaEventRecord(st,0));
+  for(int k=0; k<nreps; k++){
+    //将流和内存复制分开，保证流都是异步执行的，
+    for(int i=0; i<nstreams; i++){
+      init_array<<<blocks, threads, 0, streams[i]>>>(d_a+i*n/nstreams, d_c, niters);
+    }
+    //异步执行nstreams 的内存复制，要注意的是，
+    // 在第n个流中的内存复制是发送在当前第n个流中 之前的cuda call都完成的情况下
+    for(int i=0; i<nstreams; i++)
+      checkCudaErrors(cudaMemcpyAsync(hAligned_a+i*n/nstreams, d_a+i*n/nstreams,
+                                      nbytes/nstreams, cudaMemcpyDeviceToHost,streams[i]));
+  } 
+  checkCudaErrors(cudaEventRecord(ed,0));
+  checkCudaErrors(cudaEventSynchronize(ed));
+  checkCudaErrors(cudaEventElapsedTime(&elapsed_time, st, ed));
+  cout<<"共 "<<nstreams<<" streams: "<<elapsed_time/nreps<<endl;
+
+  //----------------
+  cout<<"--------------------"<<endl;
+  correct_data(hAligned_a, n, c*nreps*niters);  
+  
+  //free resources
+  for(int i=0; i<nstreams; i++)
+    checkCudaErrors(cudaStreamDestroy(streams[i]));
+
+  checkCudaErrors(cudaEventDestroy(st));
+  checkCudaErrors(cudaEventDestroy(ed));
+  
+  freeHostMemory(bPinGenericMem, &h_a, &hAligned_a, nbytes);
+
+  checkCudaErrors(cudaFree(d_a));
+  checkCudaErrors(cudaFree(d_c));
   
 }
